e9b7dfc583591db40b3dc4830a2cc95216c1ef49
James Doran <jysdoran@gmail.com>
2015-09-12 00:08:59 +0800

    Add "Athletes"
    They head towards the goal with an initial force

---
+++ Assets/Scripts/AthleteMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AthleteMovement : MonoBehaviour {
+	public float direction; 
+	public float baseSpeed;
+	public float speed;
+	public GameObject goal;
+	Vector2 direc;
+	Rigidbody2D rbody;
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+
+		speed = baseSpeed;
+
+
+		direc = (Vector2) (goal.transform.position - transform.position);
+		direc.Normalize ();
+		rbody.AddForce(rbody.mass * direc * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
