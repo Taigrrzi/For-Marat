55fd8d8c5984c6e1b5adf41ca112e8e4deb87d80
James Doran <jysdoran@gmail.com>
2015-09-12 18:17:44 +0800

    Add Ability Framework
    Displayed with red dots

--- Assets/Scripts/AthleteMovement.cs
+++ Assets/Scripts/AthleteMovement.cs
@@ -1,27 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AthleteMovement : MonoBehaviour {
-	public float baseSpeed;
-	public float speed;
-	public GameObject goal;
-	Vector2 direc;
-	Rigidbody2D rbody;
-	// Use this for initialization
-	void Start () {
-		rbody = GetComponent<Rigidbody2D> ();
+public class AthleteMovement : MonoBehaviour
+{
+    public float baseSpeed;
+    public float speed;
+    public GameObject goal;
+    public float abilityCooldown;
+    public float abilityChance;
+    public int abilityType;
+    float abilityTimer;
+    Vector2 direc;
+    Rigidbody2D rbody;
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
 
-		speed = baseSpeed;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
+        speed = baseSpeed;
+        abilityTimer = 0;
     }
 
-    void FixedUpdate () {
+    // Update is called once per frame
+    void Update()
+    {
+        transform.FindChild("RedDot").localScale = (new Vector2(1f, 1f)) * (abilityTimer / abilityCooldown);
+        if (abilityTimer <= 0)
+        {
+            abilityTimer = 0;
+            if (abilityChance >= Random.value / Time.deltaTime)
+            {
+                UseAbility();
+                abilityTimer = abilityCooldown;
+            }
+        }
+        else
+        {
+            abilityTimer -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
         direc = (Vector2)(goal.transform.position - transform.position);
         direc.Normalize();
         rbody.AddForce(rbody.mass * direc * speed);
+    }
+
+    void UseAbility()
+    {
+        switch (abilityType)
+        {
+            case 0:
+                Debug.Log("Ability!");
+                break;
+            default:
+                break;
+        }
     }
 }
