b33c4aead9eeeb1756766fe96dac16e15acef699
James Doran <jysdoran@gmail.com>
2015-09-19 18:43:10 +0800

    Make match starting a separate function

--- Assets/Scripts/matchControl.cs
+++ Assets/Scripts/matchControl.cs
@@ -7,15 +7,20 @@ public class matchControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	    switch (matchType)
+        StartMatch(matchType);
+	}
+
+    void StartMatch(int type)
+    {
+        switch (type)
         {
             case 0:
-                GameObject champ = (GameObject) Instantiate(Resources.Load("Athlete"));
-                champ.transform.position = new Vector3(2.2f,2.5f,0);
-                champ.GetComponent<AthleteMovement>().baseSpeed = (Random.value*10)+5;
+                GameObject champ = (GameObject)Instantiate(Resources.Load("Athlete"));
+                champ.transform.position = new Vector3(2.2f, 2.5f, 0);
+                champ.GetComponent<AthleteMovement>().baseSpeed = (Random.value * 10) + 5;
                 champ.GetComponent<AthleteMovement>().abilityChance = Random.value;
                 champ.GetComponent<AthleteMovement>().abilityType = 0;
-                champ.GetComponent<Rigidbody2D>().mass = Random.value*2.5f;
+                champ.GetComponent<Rigidbody2D>().mass = Random.value * 2.5f;
                 champ.GetComponent<AthleteMovement>().abilityCooldown = 5;
                 champ.GetComponent<AthleteMovement>().goal = GameObject.Find("Goal");
 
@@ -32,10 +37,5 @@ public class matchControl : MonoBehaviour {
                 champ.GetComponent<AthleteMovement>().opponent = champ2;
                 break;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
 }
